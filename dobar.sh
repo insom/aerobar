f79ba3837ec45231d902e15185fe9a5842350baf
Aaron Brady <aaron@insom.me.uk>
2024-04-21 23:33:58 -0400

    Solarized colors, switch to local lemonbar(1)

--- dobar.sh
+++ dobar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #./aerobar | lemonbar -b -B '#ffa500' -F '#333333' -f 9x15bold
-./aerobar | lemonbar -d -b -B '#111111' -F '#555555' -f 9x15bold -g x30 | bash
+./aerobar | ~/Bin/lemonbar -d -b -B '#586e75' -F '#93a1a1' -f 9x15bold -g x30 | bash
